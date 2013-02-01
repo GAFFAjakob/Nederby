3f68e8dcad47a44a8a6a91d6fd6450a4b1ae40a2
Jakob Nederby Nielsen <jakob@gaffa.dk>
2013-01-31 20:53:02 -0800

    JS, CSS and frontpage file

--- app/View/Pages/frontpage.ctp
+++ app/View/Pages/frontpage.ctp
@@ -1,3 +1,4 @@
 <h2>Welcome to Nederby Home Server Project</h2>
 <h3>Minecraft server</h3>
-<a href="/minecraftLogs/">Minecraft logs</a>
+<a href="/minecraftLogs/">Minecraft chat log</a><br />
+<a href="/minecrafts/map">Minecraft kort</a>
---
+++ app/webroot/js/minecraft/overview/overviewerConfig.js
@@ -0,0 +1,85 @@
+var overviewerConfig = {
+    "worlds": [
+        "world"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                38, 
+                86, 
+                233
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1359086998, 
+            "name": "Overview", 
+            "poititle": "Signs", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 8, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "world", 
+            "maxZoom": 8, 
+            "path": "http://nederby.homeserver.com/img/minecraft/overview/world-smooth_lighting"
+        }, 
+        {
+            "spawn": [
+                38, 
+                86, 
+                233
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1359086998, 
+            "name": "Cave view", 
+            "poititle": "Signs", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 8, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "world", 
+            "maxZoom": 8, 
+            "path": "http://nederby.homeserver.com/img/minecraft/overview/world-cave"
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "http://nederby.homeserver.com/img/minecraft/overview/signpost.png", 
+            "queryMarker": "http://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "http://nederby.homeserver.com/img/minecraft/overview/signpost_icon.png", 
+            "spawnMarker": "http://google-maps-icons.googlecode.com/files/home.png", 
+            "bedMarker": "http://nederby.homeserver.com/img/minecraft/overview/bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1359093223"
+    }
+};
